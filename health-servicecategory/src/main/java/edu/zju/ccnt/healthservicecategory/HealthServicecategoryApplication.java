79ee8ecf11e00de62297a609172b8d4794ac0bff
ZhangTh2 <1374039989@qq.com>
2019-03-12 10:44:24 +0800

    first commit

---
+++ health-servicecategory/src/main/java/edu/zju/ccnt/healthservicecategory/HealthServicecategoryApplication.java
@@ -0,0 +1,13 @@
+package edu.zju.ccnt.healthservicecategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServicecategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthServicecategoryApplication.class, args);
+    }
+
+}
