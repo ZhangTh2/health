0c0f4d69e2bf78c12daf65b6fded2ff6d4672a01
ZhangTh2 <1374039989@qq.com>
2019-05-16 20:22:02 +0800

    Add eureka-server

---
+++ health-center1/src/main/java/edu/zju/ccnt/health/HealthCenter1Application.java
@@ -0,0 +1,15 @@
+package edu.zju.ccnt.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class HealthCenter1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCenter1Application.class, args);
+    }
+
+}
